25816e4d983668d0e2d77e789cf0f742a202448b
rudyhuacasi <alexisitaliano16@gmail.com>
2024-07-17 17:17:01 +0200

    {UPD} added PageController and model (comic)

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index ()
+    {
+        
+    }
+    public function show()
+    {
+
+    }
+    public function create()
+    {
+
+    }
+    public function store()
+    {
+
+    }
+}
